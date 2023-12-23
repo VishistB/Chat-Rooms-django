fb513c2738f6a3e1f57c95e0a54a2b97e57bf000
Vishist Bhagabati <55060840+VishistB@users.noreply.github.com>
2023-12-23 23:50:39 +0530

    slugs for chatrooms

--- chatapp/urls.py
+++ chatapp/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.rooms),
+    path("<str:slug>",views.room)
 ]
--- chatapp/views.py
+++ chatapp/views.py
@@ -5,3 +5,7 @@ def rooms(request):
     rooms=Room.objects.all()
     context={"rooms":rooms}
     return render(request,"rooms.html",context)
+
+def room(request,slug):
+    context={"slug":slug}
+    return render(request,"room.html",context)
