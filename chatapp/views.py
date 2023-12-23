cb5a04561d680b1a21a790f6702a4a75d9add739
Vishist Bhagabati <55060840+VishistB@users.noreply.github.com>
2023-12-24 04:17:38 +0530

    language function moved to views.py

--- chatapp/views.py
+++ chatapp/views.py
@@ -1,5 +1,36 @@
 from django.shortcuts import render
 from .models import Room,Message
+from nltk import word_tokenize, pos_tag
+
+def LangFosFunc(sentence):
+    words = word_tokenize(sentence)
+    pos_tags = pos_tag(words)
+
+    adjectives = [word for word, tag in pos_tags if tag.startswith('JJ')]
+    adverbs = [word for word, tag in pos_tags if tag.startswith('RB')]
+    conjunctions = [word for word, tag in pos_tags if tag == 'CC']
+    determiners = [word for word, tag in pos_tags if tag == 'DT']
+    nouns = [word for word, tag in pos_tags if tag.startswith('NN')]
+    numbers = [word for word, tag in pos_tags if tag == 'CD']
+    prepositions = [word for word, tag in pos_tags if tag == 'IN']
+    pronouns = [word for word, tag in pos_tags if tag.startswith('PR')]
+    verbs = [word for word, tag in pos_tags if tag.startswith('VB')]
+
+    result = {
+        "Adjectives": adjectives,
+        "Adverbs": adverbs,
+        "Conjunctions": conjunctions,
+        "Determiners": determiners,
+        "Nouns": nouns,
+        "Numbers": numbers,
+        "Prepositions": prepositions,
+        "Pronouns": pronouns,
+        "Verbs": verbs
+    }
+
+    return result
+
+
 
 # Create your views here.
 def rooms(request):
@@ -11,6 +42,8 @@ def room(request,slug):
     room_name=Room.objects.get(slug=slug).name
     messages=Message.objects.filter(room=Room.objects.get(slug=slug))
     context={"slug":slug,"room_name":room_name,'messages':messages}
+
+    
     return render(request,"room.html",context)
 
 
