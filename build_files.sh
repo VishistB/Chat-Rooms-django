f86e5245d750382bc43bddd0bdeec6516cd2ea0c
Vishist Bhagabati <55060840+VishistB@users.noreply.github.com>
2023-12-24 13:24:26 +0530

    vercel experiment 2

--- build_files.sh
+++ build_files.sh
@@ -4,5 +4,8 @@
 echo "Building the project..."
 python3.9 -m pip install -r requirements.txt
 
+apt-get update
+apt-get install -y libpq-dev
+
 echo "Collect Static..."
 python3.9 manage.py collectstatic --noinput --clear
