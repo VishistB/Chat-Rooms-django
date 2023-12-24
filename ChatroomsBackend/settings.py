21d0d0cfc46c2cae4e152226db244d1cbab992fe
Vishist Bhagabati <55060840+VishistB@users.noreply.github.com>
2023-12-24 23:24:05 +0530

    deployment fix1

--- ChatroomsBackend/settings.py
+++ ChatroomsBackend/settings.py
@@ -138,13 +138,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-# STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
 # STATIC_URL = 'static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # MEDIA_URLS ='/media/'
