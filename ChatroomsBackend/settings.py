53780cdcd4a21a29c95258f5268e04c9fe7bc6c8
Vishist Bhagabati <55060840+VishistB@users.noreply.github.com>
2023-12-28 00:43:09 +0530

    db seperated from webapp

--- ChatroomsBackend/settings.py
+++ ChatroomsBackend/settings.py
@@ -96,31 +96,28 @@ WSGI_APPLICATION = "ChatroomsBackend.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.environ['DB_NAME'],
-#         'USER': os.environ['DB_USER'],
-#         'PASSWORD': os.environ['DB_PASSWORD'],
-#         'HOST': os.environ['DB_HOST'],
-#         'PORT': os.environ['DB_PORT'],
-#     }
-# }
-
-conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': conn_str_params['dbname'],
-        'HOST': conn_str_params['host'],
-        'USER': conn_str_params['user'],
-        'PASSWORD': conn_str_params['password'],
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': os.environ['DB_HOST'],
+        'PORT': os.environ['DB_PORT'],
     }
 }
+
+# conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+# conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': conn_str_params['dbname'],
+#         'HOST': conn_str_params['host'],
+#         'USER': conn_str_params['user'],
+#         'PASSWORD': conn_str_params['password'],
+#     }
+# }
 
 
 
